efd27b31a1ee3d6a2a77c152fbbba2d409d146b6
Musharuff Shaik <122172610+W-Pheonix@users.noreply.github.com>
2024-08-19 22:35:47 +0530

    Update App.jsx

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "https://hospital-management-one-xi.vercel.app/api/v1/user/admin/me",
+          "https://hospital-management-e78r.onrender.com/api/v1/user/admin/me",
           {
             withCredentials: true,
           }

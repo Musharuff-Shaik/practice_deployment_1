58316e8ceff38cf38ac97a6a3b84d2e0cb252ee5
Musharuff Shaik <122172610+W-Pheonix@users.noreply.github.com>
2024-08-19 22:59:02 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,21 +2,22 @@ import express from "express";
 import { dbConnection } from "./database/dbConnection.js";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+// import cors from "cors";
+const cors = require('cors')
 import fileUpload from "express-fileupload";
 import { errorMiddleware } from "./middlewares/error.js";
 import messageRouter from "./router/messageRouter.js";
 import userRouter from "./router/userRouter.js";
 import appointmentRouter from "./router/appointmentRouter.js";
 
+
 const app = express();
 config({ path: "./config.env" });
 
 app.use(cors());
-
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
   fileUpload({
@@ -24,6 +25,7 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
+
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/appointment", appointmentRouter);
@@ -31,4 +33,8 @@ app.use("/api/v1/appointment", appointmentRouter);
 dbConnection();
 
 app.use(errorMiddleware);
+
 export default app;
+
+
+

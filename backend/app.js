da0f8d43a8cb24eec781cc849824d1deabd747e1
Musharuff Shaik <122172610+W-Pheonix@users.noreply.github.com>
2024-08-19 22:52:55 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,9 +14,9 @@ config({ path: "./config.env" });
 
 app.use(cors());
 
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 app.use(
   fileUpload({
